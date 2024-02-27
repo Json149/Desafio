21fbd85ada30a662078e5a7f3863c1d756e53420
jason <j.molina11@hotmail.com>
2024-02-27 09:10:09 -0600

    first commit

---
+++ src/test/java/sv/edu/udb/desafio/DesafioApplicationTests.java
@@ -0,0 +1,13 @@
+package sv.edu.udb.desafio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
